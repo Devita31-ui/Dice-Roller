53c8a227304001d6faf14444a3da8cb2f1887792
Devita31-ui <depitaputri.kepsu@gmail.com>
2025-10-06 09:25:57 +0700

    Dice Roll

--- app/src/main/java/pnm/devita/ti/diceroller/MainActivity.kt
+++ app/src/main/java/pnm/devita/ti/diceroller/MainActivity.kt
@@ -3,30 +3,26 @@ package pnm.devita.ti.diceroller
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import pnm.devita.ti.diceroller.ui.theme.DiceRollerTheme
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
+import pnm.devita.ti.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,22 +32,24 @@ class MainActivity : ComponentActivity() {
                 DiceRollerApp()
             }
         }
-
     }
 }
-@Preview
+
+@Preview(showBackground = true)
 @Composable
-fun DiceRollerApp(){
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+fun DiceRollerApp() {
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
 }
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
-    var result by remember { mutableStateOf(1) }
-    val imageResources = when (result){
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+
+    val imageResource = when (result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
         3 -> R.drawable.dice_3
@@ -59,18 +57,20 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier){
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column (
+
+    Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-            painter = painterResource(imageResources),
+            painter = painterResource(id = imageResource),
             contentDescription = result.toString()
         )
+
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random()}) {
-            Text(stringResource(R.string.roll))
+
+        Button(onClick = { result = (1..6).random() }) {
+            Text(text = stringResource(id = R.string.roll))
         }
     }
-
 }
